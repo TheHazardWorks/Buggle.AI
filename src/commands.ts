9f982f652b03980b1c9091c668625b7c63afcec9
Seven <radiodarkgame@gmail.com>
2019-06-12 12:34:16 -0400

    Added "purge" to commands

--- src/commands.ts
+++ src/commands.ts
@@ -3,7 +3,9 @@ import about from './commands/about';
 import user from './commands/user';
 import emoji from './commands/emoji';
 import latency from './commands/latency';
+import purge from './commands/purge';
 
 export default [
-    quote, about, user, emoji, latency
+    quote, about, user, emoji, latency,
+    purge
 ];
