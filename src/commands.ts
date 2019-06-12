18cedc02cc508abf9c2ecc5351561a674e2bf564
Seven <radiodarkgame@gmail.com>
2019-06-12 12:26:17 -0400

    Added "latency" to commands

--- src/commands.ts
+++ src/commands.ts
@@ -2,7 +2,8 @@ import quote from './commands/quote';
 import about from './commands/about';
 import user from './commands/user';
 import emoji from './commands/emoji';
+import latency from './commands/latency';
 
 export default [
-    quote, about, user, emoji
+    quote, about, user, emoji, latency
 ];
